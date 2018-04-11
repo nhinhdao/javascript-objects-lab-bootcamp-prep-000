3ce1a4bb703434e4b34b3345c92dabc8a9f3ab43
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-04-11 23:18:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function destructivelyUpdateObjectWithKeyAndValue(obj, key, value){
 }
 
 function deleteFromObjectByKey(obj, key){
-   var newObj = Object.ass({}, obj);
+   var newObj = Object.assign({}, obj);
    delete newObj[key];
    return newObj;
 }
