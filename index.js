2e81bda2aceeff4ed2e5f97f95c10d37088ac2c3
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-04-11 23:10:46 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,22 @@
+var recipes = new Object{};
+
+function updateObjectWithKeyAndValue(object, key, value){
+   var newobj = obj.assign({}, obj, {[key]:value})
+   return newobj;
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+   obj.assign(obj, {[key]:value})
+   return obj;
+}
+
+function deleteFromObjectByKey(object, key){
+   var newObj = obj;
+   delete newObj[key];
+   return newObj;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+   delete obj[key];
+   return obj;
+}
